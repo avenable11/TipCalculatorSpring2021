2b31f33569c72206386f4dc07686a7ba0c8b76c3
Angela K Venable <avenable11@ivytech.edu>
2021-02-24 21:15:53 -0500

    State after 2/24/2021 lecture

--- app/src/main/java/edu/ivytech/tipcalculatorspring2021/MainActivity.kt
+++ app/src/main/java/edu/ivytech/tipcalculatorspring2021/MainActivity.kt
@@ -5,43 +5,99 @@ package edu.ivytech.tipcalculatorspring2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.*
+import com.google.android.material.snackbar.Snackbar
 
 
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
+import edu.ivytech.tipcalculatorspring2021.databinding.ActivityMainBinding
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.text.NumberFormat
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
     private lateinit var billAmountET:EditText
     private lateinit var tipPercentET:EditText
     var tipPercent = .15
-    private lateinit var upBtn:ImageButton
-    private lateinit var downBtn:ImageButton
     private lateinit var calculateBtn:Button
+    private lateinit var binding:ActivityMainBinding
+    private val ROUND_NONE = 0
+    private val ROUND_TIP = 1
+    private val ROUND_TOTAL = 2
+    private var rounding = ROUND_NONE
+    private var split = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        billAmountET = findViewById(R.id.billAmountEditText)
-        tipPercentET = findViewById(R.id.tipPercentEditText)
-       // upBtn = findViewById(R.id.upBtn)
-        //upBtn.setOnClickListener {
-          //  tipPercent = tipPercent + .01
-            //tipPercent * 100
-            //tipPercentET.setText((tipPercent * 100).toString())
-            //calculateAndDisplay()
-        //}
-        //downBtn = findViewById(R.id.downBtn)
-        //downBtn.setOnClickListener {
-          //  tipPercent -= 0.01
-            //calculateAndDisplay()
-        //}
-        calculateBtn = findViewById(R.id.calculateBtn)
+        billAmountET = binding.billAmountEditText
+        tipPercentET = binding.tipPercentEditText
+
+        calculateBtn = binding.calculateBtn
         calculateBtn.setOnClickListener { calculateAndDisplay() }
+        binding.seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                tipPercent = progress/100.0
+                tipPercentET.setText(progress.toString())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
+        binding.radioGroup.check(R.id.noRoundRadio)
+        binding.radioGroup.setOnCheckedChangeListener(object: RadioGroup.OnCheckedChangeListener{
+            override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+                when(checkedId) {
+                    R.id.noRoundRadio -> rounding = ROUND_NONE
+                    R.id.roundTipRadio -> rounding = ROUND_TIP
+                    R.id.roundTotalRadio -> rounding = ROUND_TOTAL
+                }
+                calculateAndDisplay()
+            }
+        })
+
+        val items = resources.getStringArray(R.array.split_array)
+        val adapter = ArrayAdapter(this,R.layout.list_item,items)
+        (binding.splitBillET as? AutoCompleteTextView)?.setAdapter(adapter)
+        binding.splitBillET.setText(items[0], false)
+        binding.splitBillET.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                for(i in items.indices) {
+                    if(items[i] == s.toString()) {
+                        split = i + 1
+                        break
+                    }
+                }
+                if(split > 1) {
+                    binding.eachPayLayout.visibility = View.VISIBLE
+                } else {
+                    binding.eachPayLayout.visibility = View.GONE
+                }
+                calculateAndDisplay()
+
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
 
 
     }
@@ -52,7 +108,15 @@ class MainActivity : AppCompatActivity() {
             billAmount = billAmountStr!!.toDouble()
         }
         var tipAmount = billAmount * tipPercent
+        if(rounding == ROUND_TIP){
+            tipAmount = round(tipAmount)
+        }
+
         var totalAmount = billAmount + tipAmount
+        if(rounding == ROUND_TOTAL) {
+            totalAmount = round(totalAmount)
+            tipAmount = totalAmount - billAmount
+        }
         var currencyFormat = NumberFormat.getCurrencyInstance()
         var percentFormat = NumberFormat.getPercentInstance()
         var symbol = DecimalFormatSymbols()
@@ -66,6 +130,11 @@ class MainActivity : AppCompatActivity() {
         totalAmountEditText.setText(currencyFormat.format(totalAmount))
 
         tipPercentET.setText(percentFormat.format(tipPercent))
+
+        if(split > 1){
+            var splitAmount = totalAmount/split
+            binding.eachPayText.setText(currencyFormat.format(splitAmount))
+        }
 
 
     }
